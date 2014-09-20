464ec2e75af3e9106fa190dc0ef378ac9b59f487
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-09-20 13:29:14 +0200

    refactor plugin; remove hard-coded javac call.

--- src/leiningen/thriftc.clj
+++ src/leiningen/thriftc.clj
@@ -1,159 +1,131 @@
-(ns ^{ :doc "A Thrift Plugin for Leiningen"
-       :author "Yannick Scherer" }
+(ns ^{:doc "A Thrift Plugin for Leiningen"
+      :author "Yannick Scherer"}
   leiningen.thriftc
   (:require [robert.hooke]
-            [leiningen.javac :only [javac]]
+            [leiningen
+             [clean :as c]
+             [javac :as j]]
             [leiningen.core.main :as main]
             [clojure.tools.reader.edn :as edn :only [read-string]]
             [clojure.java.io :as io]
             [clojure.java.shell :as shell])
   (:import java.io.File))
 
-;; ## Print
-
-(def ^:private ^:dynamic *verbose* nil)
-
-(defn- verbose*
-  "Print Log Output."
-  [& msg]
-  (when *verbose*
-    (print (apply str msg))
-    (.flush *out*)))
-
-(defn- verbose
-  "Print Log Output with Linefeed."
-  [& msg]
-  (when *verbose*
-    (println (apply str msg))
-    (.flush *out*)))
-
 ;; ## Helpers
+
+(def ^:private tag "[thriftc]")
 
 (defn- read-edn
   "Read EDN file."
-  [target-file]
-  (let [f (io/file target-file)]
-    (when (.exists f)
-      (edn/read-string (slurp f)))))
+  [^File target-file]
+  (when (.exists target-file)
+    (edn/read-string (slurp target-file))))
 
-(defn- thrift-sources
+(defn- collect-thrift-sources
   "Find Thrift Sources in the given directories."
-  [dirs ]
+  [{:keys [source-paths]}]
   (mapcat
     (fn [dir]
       (let [^File f (io/file dir)]
         (filter #(-> ^File % (.getName) (.endsWith ".thrift")) (file-seq f))))
-    dirs))
+    source-paths))
 
-(defn- check-thrift-available
+(defn- assert-thrift-available!
   "Check if the given Thrift executable is available."
-  [thrift-executable]
+  [{:keys [path]}]
   (try
-    (do
-      (shell/sh thrift-executable "-version")
+    (let [{:keys [exit out]} (shell/sh path "-version")]
+      (when out
+        (main/debug tag (.trim (str out))))
       true)
-    (catch Exception _ nil)))
+    (catch Exception _ nil
+      (main/abort tag "could not find thrift at:" path))))
 
-(defn- compile-thrift
-  [thrift out-dir thrift-java-opts src-file]
-  (try
-    (let [{:keys[exit out err]} (shell/sh 
-                                  thrift "-r"
-                                  "--gen" "java" #_(str "java:" thrift-java-opts) 
-                                  "-out" out-dir
-                                  src-file)]
-      (when-not (zero? exit)
-        (verbose)
-        (println (str "Could not compile `" src-file "':"))
-        (println err))
-      (zero? exit))
-    (catch Exception ex
-      (verbose)
-      (.printStackTrace ex)
-      nil))) 
+(defn- compile-thrift-file!
+  [{:keys [path target-path java-gen-opts]} src-file]
+  (main/debug tag "*compile*" (.getPath ^File src-file))
+  (let [full-target-path (.getCanonicalPath ^File target-path)
+        full-source-path (.getCanonicalPath ^File src-file)
+        {:keys[exit out err]} (shell/sh
+                                path "-r"
+                                "--gen" "java" #_(str "java:" java-gen-opts)
+                                "-out" full-target-path
+                                full-source-path)]
+    (when-not (zero? exit)
+      (main/info tag "could not compile thrift file:" full-source-path)
+      (main/info err))
+    (zero? exit)))
 
-(defn- compile-stale-thrift-files
-  "Compile all stale Thrift files based on a compile function, a file with
-   last-modified information and a seq of source files."
-  [compile! mod-file src-files & {:keys[force-compile]}]
-  (let [last-modified (or (read-edn mod-file) {})]
-    (loop [src-files src-files
-           last-modified last-modified]
-      (if-let [[src-file & rst] (seq src-files)]
-        (let [p (.getPath src-file)
-              m (get last-modified p)]
-          (verbose* (str  "Generating Java Sources for `" p "' ... "))
-          (if (and (not force-compile) m (<= (.lastModified src-file) m))
-            (do
-              (verbose "No Changes.")
-              (recur rst last-modified))
-            (when (compile! p)
-              (verbose "OK.")
-              (recur rst (assoc last-modified p (.lastModified src-file))))))
-        (do
-          (spit mod-file last-modified)
-          true)))))
+(defn- compile-stale-thrift-files!
+  [{:keys [modified-file force-compile] :as opts} src-files]
+  (let [last-modified (or (read-edn modified-file) 0)
+        last-modified (if (number? last-modified) last-modified 0)]
+    (doseq [^File src-file src-files]
+      (let [ts (.lastModified src-file)]
+        (if (> ts last-modified)
+          (when-not (compile-thrift-file! opts src-file)
+            (main/abort tag "compilation failed."))
+          (main/debug tag "*keeping*" (.getPath src-file)))))
+    (spit modified-file (System/currentTimeMillis))))
 
-;; ## Get Thrift Data
+(defn- compile-all-sources!
+  [{:keys [target-path] :as opts}]
+  (if-let [src-files (seq (collect-thrift-sources opts))]
+    (do
+      (main/debug tag "sources:" (pr-str (vec src-files)))
+      (.mkdirs ^File target-path)
+      (compile-stale-thrift-files! opts src-files))
+    (main/debug tag "no source files found.")))
 
-(defn- thrift-project
-  [project]
-  (let [data (:thriftc project {})
-        target-dir (:target-path project "target")]
-    (-> {}
-      (assoc :path (:path data "thrift"))
-      (assoc :source-paths (:source-paths data ["src/thrift"]))
-      (assoc :java-gen-opts (:java-opts data "bean,hashcode"))
-      (assoc :javac-opts (:javac-opts data []))
-      (assoc :target-path (str target-dir "/thrift-java"))
-      (assoc :modified-file (str target-dir "/.lein-thriftc-modified"))
-      (assoc :force-compile (:force-compile data nil)))))
+;; ## Settings
+
+(def ^:private default-settings
+  {:path "thrift"
+   :source-paths ["src/thrift"]
+   :java-gen-opts "bean/hashcode"
+   :force-compile false})
+
+(defn thrift-settings
+  [{:keys [thriftc target-path root]}]
+  (->> (select-keys thriftc (keys default-settings))
+       (merge default-settings)
+       (merge
+         {:target-path   (io/file target-path "thrift-java")
+          :modified-file (io/file root ".lein-thriftc-modified")})))
 
 ;; ## Leiningen Command
 
 (defn thriftc
-  "Generate Java Sources for Thrift Files.
-   
-   project.clj:
+  "Generate Java Sources for Thrift Files. Options include:
 
-    :thriftc { :path \"thrift\"
+     :thriftc {:path \"thrift\"
                :source-paths [\"src/thrift\"]
                :java-gen-opts \"bean,hashcode\"
-               :javac-opts [] 
-               :force-compile false }
-  "
+               :force-compile false}
+   "
   [project & args]
   (when-not (:root project)
-    (println "Can only be run inside a Project!")
-    (main/exit 1))
-  (let [thrift-project (thrift-project project)
-        thrift (:path thrift-project "thrift")]
-    (when-not (check-thrift-available thrift)
-      (println "Could not find Thrift at:" thrift)
-      (main/exit 1))
-    (let [verbose? (= (first args) ":verbose")
-          src-dirs (:source-paths thrift-project)
-          java-dir (:target-path thrift-project)
-          mod-file (:modified-file thrift-project)
-          java-opts (:java-gen-opts thrift-project)
-          javac-opts (:javac-opts thrift-project)
-          src-files (thrift-sources src-dirs)
-          compile! (partial compile-thrift thrift java-dir java-opts)]
-      (binding [*verbose* (= (first args) ":verbose")]
-        (when (seq src-files)
-          (.mkdirs (io/file java-dir))
-          (verbose "------------------------------ Compiling Thrift ------------------------------") 
-          (verbose "Thrift Path:    " thrift)
-          (verbose "Source Dirs:    " src-dirs)
-          (verbose "Java Output:    " java-dir)
-          (verbose)
-          (let [success? (compile-stale-thrift-files 
-                           compile! mod-file src-files
-                           :force-compile (:force-compile thrift-project))]
-            (verbose "------------------------------------------------------------------------------")
-            (when-not success?
-              (main/exit 1)))
-          (leiningen.javac/javac 
-            (-> project
-              (update-in [:java-source-paths] #(conj (vec %) java-dir))
-              (update-in [:javac-options] #(reduce conj (vec %) javac-opts)))))))))
+    (main/abort "Can only be run inside a Project!"))
+  (let [opts (thrift-settings project)]
+    (main/debug tag "options:" (pr-str opts))
+    (assert-thrift-available! opts)
+    (compile-all-sources! opts)))
+
+;; ## Hook
+
+(defn activate
+  "Register thrift compilation as a hook before javac."
+  []
+  (->> (fn [f project & args]
+         (let [{:keys [modified-file]} (thrift-settings project)]
+           (when (.isFile modified-file)
+             (.delete modified-file))
+           (apply f project args)))
+       (robert.hooke/add-hook #'c/clean :thriftc))
+  (->> (fn [f project & args]
+         (thriftc project)
+         (let [{:keys [target-path javac-opts]} (thrift-settings project)
+               path (.getPath ^File target-path)
+               project' (update-in project [:java-source-paths] concat [path])]
+           (apply f project' args)))
+       (robert.hooke/add-hook #'j/javac :thriftc)))
