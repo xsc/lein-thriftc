1bac7da668d316ff3ae74548d721fd5bcbec6fe9
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-04-26 12:30:49 +0200

    Version 0.2.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-thriftc "0.2.2"
+(defproject lein-thriftc "0.2.3-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "Leiningen Plugin for Apache Thrift"
   :url "https://github.com/xsc/lein-thriftc"
