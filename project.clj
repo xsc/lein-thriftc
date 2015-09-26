e1eaef458f17b15ce25436ea7229307ecdd6c54b
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-09-26 12:40:13 +0200

    switch to MIT License.

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :min-lein-version "2.0.0"
   :description "Leiningen Plugin for Apache Thrift"
   :url "https://github.com/xsc/lein-thriftc"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :year 2015
+            :key "mit"}
   :dependencies [[org.clojure/tools.reader "0.8.9"]]
   :eval-in-leiningen true)
